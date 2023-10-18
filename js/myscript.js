d25a57409b5f0d6010b1351c05477f8f2c8761bf
tedescohtml <143408726+tedescohtml@users.noreply.github.com>
2023-10-18 14:42:59 +0000

    atualização

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,22 @@
 function gebi(id) {
-    return document.getElementById
+    return document.getElementById(id)
 }
 
 function multiplicar() {
-    let numero1 = gebi("n1").value
+    let numeroN1 = parseInt(gebi("n1").value)
+    let numeroN2 = parseInt(gebi("n2").value)
+    let numeroN3 = parseInt(gebi("n3").value)
+    let numeroN4 = parseInt(gebi("n4").value)
+    let numeroN5 = parseInt(gebi("n5").value)
+    let numeroN6 = parseInt(gebi("n6").value)
+    let numeroN7 = parseInt(gebi("n7").value)
+    let numeroN8 = parseInt(gebi("n8").value)
+    let numeroN9 = parseInt(gebi("n9").value)
+    let numeroN10 = parseInt(gebi("n10").value)
+    let numeroN11 = parseInt(gebi("n11").value)
+    let numeroN12 = parseInt(gebi("n12").value)
+    let numeroN13 = parseInt(gebi("n13").value)
+    let numeroN14 = parseInt(gebi("n14").value)
+    let numeroN15 = parseInt(gebi("n15").value)
+    let numeroN16 = parseInt(gebi("n16").value)
 }
